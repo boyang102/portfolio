0c4f97c1757aa7428681ed94e03e8ef5acacf3a4
boyang102 <66711671+boyang102@users.noreply.github.com>
2025-10-13 18:16:43 -0700

    lab3.2

--- global.js
+++ global.js
@@ -1,5 +1,16 @@
-console.log('IT’S ALIVE!');
+console.log("IT’S ALIVE!");
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Step 2: Automatic current page link
+let navLinks = $$("nav a");
+
+// 找到当前页面的链接（匹配 host + pathname）
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// 如果找到匹配项，就自动加上 current 类
+currentLink?.classList.add("current");
