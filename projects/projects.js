a87c7514709b65c6dff1775c6c51d4f7390e0974
boyang102 <66711671+boyang102@users.noreply.github.com>
2025-10-21 14:51:34 -0700

    ok

--- projects/projects.js
+++ projects/projects.js
@@ -2,7 +2,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 async function main() {
   const projectsContainer = document.querySelector('.projects');
-  const projects = await fetchJSON('/portfolio/lib/projects.json'); // ✅ GitHub Pages 路径
+  const projects = await fetchJSON('/portfolio/lib/projects.json'); // ✅ GitHub Pages 用这个路径
 
   renderProjects(projects, projectsContainer, 'h2');
 
