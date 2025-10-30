1f349f40c482ce0a43da06b4516139e8b748ad6d
boyang102 <66711671+boyang102@users.noreply.github.com>
2025-10-30 14:04:39 -0700

    ok

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,5 @@
 // projects.js
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 // Select the SVG container and set coordinate system
 const svg = d3
@@ -18,17 +18,13 @@ let sliceGenerator = d3.pie();
 // Compute arc data
 let arcData = sliceGenerator(data);
 
-// Convert arcs to path data
-let arcs = arcData.map((d) => arcGenerator(d));
-
 // D3 color palette
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 // Append slices to SVG
-arcs.forEach((arc, idx) => {
-  svg
-    .append('path')
-    .attr('d', arc)
+arcData.forEach((d, idx) => {
+  svg.append('path')
+    .attr('d', arcGenerator(d))
     .attr('fill', colors(idx))
     .attr('stroke', 'white')
     .attr('stroke-width', 0.5);
